516510feba61c98e42a7eea8f92ed2b036e34e9e
indra467 <indrakgikg1919@gmail.com>
2023-03-15 01:12:14 +0530

    Updated new

--- app/src/main/java/com/example/organic_farming/MainActivity.kt
+++ app/src/main/java/com/example/organic_farming/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.organic_farming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.organic_farming.databinding.ActivityMainBinding
+import android.widget.PopupMenu
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title = when(destination.id){
+                    R.id.cardFragment -> "My Items"
+                    R.id.homeFragment -> "My Home"
+                    R.id.moreFragment -> "Seller"
+                    else -> "Organic_Farming"
+                }
+            }
+
+        })
     }
 }
